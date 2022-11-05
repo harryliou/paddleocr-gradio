b3f8222373bfbccc0c50dc72ba77bc37d4df28ea
Harry Liou <Harry_Liou@asus.com>
2022-11-05 10:48:50 +0800

    Add paddocr rudely

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # paddleocr-gradio
+
+## Init
+    pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+    pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,14 +1,50 @@
+import cv2
 import gradio as gr
+from ocr.predict_system import TextSystem
+import ocr.utility as utility
+import os
+from PIL import Image
 
-def predict(img):
-    return img
+res_path = 'res'
+
+def predict(img, drop_score):
+    args = utility.parse_args()
+    args.drop_score = drop_score
+    args.det_model_dir = os.path.join(res_path, 'ch_PP-OCRv3_det_infer')
+    args.use_angle_cls = True
+    args.cls_model_dir = os.path.join(res_path, 'ch_ppocr_mobile_v2.0_cls_infer')
+    args.rec_model_dir = os.path.join(res_path, 'chinese_cht_PP-OCRv3_rec_infer')
+    args.rec_char_dict_path = os.path.join(res_path, 'chinese_cht_dict.txt')
+    args.vis_font_path = os.path.join(res_path, 'simfang.ttf')
+    args.use_gpu = False
+
+    text_sys = TextSystem(args)
+    dt_boxes, rec_res, time_dict = text_sys(img)
+    image = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    boxes = dt_boxes
+    txts = [rec_res[i][0] for i in range(len(rec_res))]
+    scores = [rec_res[i][1] for i in range(len(rec_res))]
+    draw_img = utility.draw_ocr_box_txt(
+        image,
+        boxes,
+        txts,
+        scores,
+        drop_score=drop_score,
+        font_path=args.vis_font_path
+    )
+    print('time spent: {}'.format(time_dict))
+    return draw_img
 
 demo = gr.Interface(
     predict,
-    inputs=[gr.Image()],
+    inputs=[
+        gr.Image(),
+        gr.Slider(0, 1, 0.7)
+    ],
     outputs='image',
     title='PaddleOCR Demo',
-    examples=[['meme.jpg']]
+    examples=[['meme.jpg']],
+    allow_flagging='never'
 )
 
-demo.launch()
+demo.launch(share=True)
